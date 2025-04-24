887ea3d3bc1b0fe6440a0d150bca7af086070f29
airishGuy <airish1219here@gmail.com>
2025-04-24 16:48:50 +0100

    ownership done

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,15 @@ fn main() {
 
 
 
+    let x = 5;
+    let y = x;
+    println!("{x}");
+    println!("{y}");
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{s1}");
+    println!("{s2}");
 
 
 }
